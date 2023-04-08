51a2f3e74c24e998fca57387bd335bdcf33431df
nizhantiw <nizhantiw@gmail.com>
2023-04-08 19:33:26 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
+function Calculator() {
+  const { search } = useLocation();
+  const { num1, num2, op } = Object.fromEntries(
+    new URLSearchParams(search)
+  );
 
-export const Calculator = () =>{
+  const calculate = () => {
+    const n1 = Number(num1);
+    const n2 = Number(num2);
 
-    return(
-        
-    )
+    switch (op) {
+      case "+":
+        return n1 + n2;
+      case "-":
+        return n1 - n2;
+      case "*":
+        return n1 * n2;
+      case "/":
+        return n1 / n2;
+      case "%":
+        return n1 % n2;
+      default:
+        return NaN;
+    }
+  };
+
+  return (
+    <div>
+      <p>Your calculation result is:</p>
+      <span id="calc-result">{calculate()}</span>
+    </div>
+  );
 }
+
+export default Calculator;
